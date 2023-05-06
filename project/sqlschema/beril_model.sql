8a495d481f88c4d1fc55a1d26162e1524ada1207
Mark A. Miller <MAM@lbl.gov>
2023-05-05 21:40:49 -0400

    metaphysics of upper classes and properties

--- project/sqlschema/beril_model.sql
+++ project/sqlschema/beril_model.sql
@@ -1,6 +1,30 @@
 
 
+CREATE TABLE "InformationArtifact" (
+	id TEXT NOT NULL, 
+	name TEXT, 
+	description TEXT, 
+	size_in_bytes INTEGER, 
+	md5 TEXT, 
+	url TEXT, 
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE "MaterialEntity" (
+	id TEXT NOT NULL, 
+	name TEXT, 
+	description TEXT, 
+	PRIMARY KEY (id)
+);
+
 CREATE TABLE "NamedThing" (
+	id TEXT NOT NULL, 
+	name TEXT, 
+	description TEXT, 
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE "NonProcess" (
 	id TEXT NOT NULL, 
 	name TEXT, 
 	description TEXT, 
@@ -12,6 +36,7 @@ CREATE TABLE "Person" (
 	name TEXT, 
 	description TEXT, 
 	primary_email TEXT, 
+	birth_date DATE, 
 	age_in_years INTEGER, 
 	vital_status VARCHAR(7), 
 	PRIMARY KEY (id)
@@ -20,4 +45,20 @@ CREATE TABLE "Person" (
 CREATE TABLE "PersonCollection" (
 	entries TEXT, 
 	PRIMARY KEY (entries)
+);
+
+CREATE TABLE "Process" (
+	id TEXT NOT NULL, 
+	name TEXT, 
+	description TEXT, 
+	inputs TEXT, 
+	outputs TEXT, 
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE "Observation" (
+	raw_value TEXT, 
+	"MaterialEntity_id" TEXT, 
+	PRIMARY KEY (raw_value, "MaterialEntity_id"), 
+	FOREIGN KEY("MaterialEntity_id") REFERENCES "MaterialEntity" (id)
 );
